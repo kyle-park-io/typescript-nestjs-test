4d9f06b6f7d9c2eb9c233c1c63c55432478063a2
kyle-park-io <andy3638@naver.com>
2024-09-20 22:13:04 +0900

    chore: apply project configuration templates

---
+++ git.sh
@@ -0,0 +1,2 @@
+git config --local user.name kyle-park-io
+git config --local user.email andy3638@naver.com
---
+++ init.sh
@@ -0,0 +1,7 @@
+yarn
+yarn init -y
+yarn
+yarn upgrade
+yarn run husky-postinstall
+npx sort-package-json
+yarn
